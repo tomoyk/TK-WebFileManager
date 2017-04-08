af0c1d2d27276ae169c8c66817cc56c8dcd4ed09
tomoyk <tmyk.kym@gmail.com>
2017-04-09 00:21:07 +0900

    modified upload.php

--- php-dev/upload.php
+++ php-dev/upload.php
@@ -1,5 +1,20 @@
 <?php
 
-// code
+/*
+1. 既存のファイルの有無の確認
+2. ファイルがアップロードされたものか確認
+3. パーミッションの確認
+*/
+
+echo $_FILES['fname']['name'];
+// echo ($_FILES['userfile']['tmp_name']);
+
+// ファイルがアップロードされたものかチェック
+if( is_uploaded_file($_FILES['fname']['tmp_name']) ) {
+  echo 'ファイル'.$FILES['userfile']['name'].'をアップロードしました.';
+} else {
+  echo '失敗しました.';
+}
+
 
 ?>
