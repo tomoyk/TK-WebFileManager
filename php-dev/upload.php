134c942aa2dc6fed08caac91cc419195f01870c5
tomoyk <tmyk.kym@gmail.com>
2017-04-12 22:37:56 +0900

    modified a little bit

--- php-dev/upload.php
+++ php-dev/upload.php
@@ -6,15 +6,40 @@
 3. パーミッションの確認
 */
 
-echo $_FILES['fname']['name'];
-// echo ($_FILES['userfile']['tmp_name']);
+echo $_FILES['fname']['name']."\n";
 
-// ファイルがアップロードされたものかチェック
-if( is_uploaded_file($_FILES['fname']['tmp_name']) ) {
-  echo 'ファイル'.$FILES['userfile']['name'].'をアップロードしました.';
-} else {
-  echo '失敗しました.';
+var_dump( $_FILES['fname']['name'] );
+
+$path = realpath(null);
+
+for($i=0;$i<count($_FILES['fname']['name']);$i++) {
+
+  $crt_name = basename($_FILES['fname']['name'][$i])."\n";
+  $tmp_name = $_FILES['fname']['tmp_name'][$i]."\n";
+
+  echo $tmp_name. "\n" .$path."/test-files/".$crt_name;
+  move_uploaded_file($tmp_name, $path."/test-files/".$crt_name);
+
+  if( is_uploaded_file($path."/test-files/".$crt_name) ) {
+    echo 'アップロードに成功しました.';
+  } else {
+    echo 'アップロードに失敗しました.';
+  }
 }
+
+/*
+$fname = $_FILES['fname']['name'];
+$tmp_name = $_FILES['fname']['tmp_name'];
+$fname = basename($fname);
+
+
+if( move_uploaded_file($tmp_fname, "test-files/$fname") ){
+
+  echo "TRUE\n";
+
+}
+*/
+
 
 
 ?>
